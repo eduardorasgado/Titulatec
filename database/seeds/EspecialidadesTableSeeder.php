93bc14d22c116e7c3cd84f391c9aba3616f1611a
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-10-22 14:58:45 -0500

    se han realizado los seeders de las especialidades y los planes de estudios que faltaban

--- database/seeds/EspecialidadesTableSeeder.php
+++ database/seeds/EspecialidadesTableSeeder.php
@@ -15,83 +15,114 @@ class EspecialidadesTableSeeder extends Seeder
         // cada una de las especialidades que son introducidas aqui
         // van a depender de las academias ya existentes
         DB::table('especialidades')->insert([
-            'nombre' => 'ARQUITECTURA',
+            'nombre' => 'ARQUITECTURA', // ----------------------- 1
             'estado' => true,
             'id_academia' => 3,
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
         DB::table('especialidades')->insert([
-            'nombre' => 'CONTADOR PÚBLICO',
+            'nombre' => 'CONTADOR PÚBLICO', // ----------------------- 2
             'estado' => true,
             'id_academia' => 6,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA CIVIL',
+            'nombre' => 'INGENIERÍA CIVIL', // ----------------------- 3
             'estado' => true,
             'id_academia' => 3,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA ELÉCTRICA',
+            'nombre' => 'INGENIERÍA ELÉCTRICA', // ----------------------- 4
             'estado' => true,
             'id_academia' => 5,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA ELECTROMECÁNICA',
+            'nombre' => 'INGENIERÍA ELECTROMECÁNICA', // ----------------------- 5
             'estado' => true,
             'id_academia' => 2,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA EN GESTIÓN EMPRESARIAL',
+            'nombre' => 'INGENIERÍA EN GESTIÓN EMPRESARIAL', // ----------------------- 6
             'estado' => true,
             'id_academia' => 6,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA INDUSTRIAL',
+            'nombre' => 'INGENIERÍA INDUSTRIAL', // ----------------------- 7
             'estado' => true,
             'id_academia' => 4,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA INFORMÁTICA',
+            'nombre' => 'INGENIERÍA INFORMÁTICA', // ----------------------- 8
             'estado' => true,
             'id_academia' => 1,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA MECATRÓNICA',
+            'nombre' => 'INGENIERÍA MECATRÓNICA', // ----------------------- 9
             'estado' => true,
             'id_academia' => 5,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA MECÁNICA',
+            'nombre' => 'INGENIERÍA MECÁNICA', // ----------------------- 10
             'estado' => true,
             'id_academia' => 2,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'INGENIERÍA EN SISTEMAS COMPUTACIONALES',
+            'nombre' => 'INGENIERÍA EN SISTEMAS COMPUTACIONALES', // ----------------------- 11
             'estado' => true,
             'id_academia' => 1,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'LICENCIATURA EN CONTADURÍA',
+            'nombre' => 'LICENCIATURA EN CONTADURÍA', // ----------------------- 12
             'estado' => true,
             'id_academia' => 6,
             'created_at' => date("Y-m-d H:i:s")
         ]);
         DB::table('especialidades')->insert([
-            'nombre' => 'LICENCIATURA EN INFORMÁTICA',
+            'nombre' => 'LICENCIATURA EN INFORMÁTICA', // ----------------------- 13
             'estado' => true,
             'id_academia' => 1,
             'created_at' => date("Y-m-d H:i:s")
         ]);
+        DB::table('especialidades')->insert([
+            'nombre' => 'INGENIERIA CIVIL EN DESARROLLO DE LA COMUNIDAD', // ----------------------- 14
+            'estado' => true,
+            'id_academia' => 3,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('especialidades')->insert([
+            'nombre' => 'INGENIERIA CIVIL EN VIAS TERRESTRES', // ----------------------- 15
+            'estado' => true,
+            'id_academia' => 3,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('especialidades')->insert([
+            'nombre' => 'INGENIERIA ELÉCTRICA EN POTENCIA', // ----------------------- 16
+            'estado' => true,
+            'id_academia' => 5,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('especialidades')->insert([
+            'nombre' => 'INGENIERÍA ELECTROMECÁNICA EN PLANTA Y MANTENIMIENTO', // ----------------------- 17
+            'estado' => true,
+            'id_academia' => 2,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('especialidades')->insert([
+            'nombre' => 'INGENIERÍA INDUSTRIAL EN PRODUCCIÓN', // ----------------------- 18
+            'estado' => true,
+            'id_academia' => 4,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
     }
 }
--- database/seeds/PlanDeEstudiosTableSeeder.php
+++ database/seeds/PlanDeEstudiosTableSeeder.php
@@ -43,10 +43,29 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'id_especialidad' => 2, // contador publico
             'created_at' => date("Y-m-d H:i:s")
         ]);
-
-
+        DB::table('plan_estudios')->insert([
+            'clave' => 'ICIV-1991-272',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 3, // ing civil
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'ICIV-1993-209',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 3, // ing civil
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
         DB::table('plan_estudios')->insert([
             'clave' => 'ICIV-1993-289',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 3, // ing civil
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'ICIV-203-82',
             'is_actual' => false,
             'estado' => true,
             'id_especialidad' => 3, // ing civil
@@ -67,7 +86,36 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
-
+        //---------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IC-74-075',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 14, // ing civil desarrollo de la comunidad
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IC-74-095',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 15, // ing civil en vias terrestrees
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        //----------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IE-73-074',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 4, // ing electrica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IELE-1985-257',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 4, // ing electrica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
         DB::table('plan_estudios')->insert([
             'clave' => 'IELEC-1993-290',
             'is_actual' => false,
@@ -89,8 +137,43 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'id_especialidad' => 4, // ing electrica
             'created_at' => date("Y-m-d H:i:s")
         ]);
-
-
+        //------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IE-73-120',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 16, // ing electrica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IELP-206-82',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 16, // ing electrica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        //------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IEME-1991-274',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 5, // ing electromecanica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IELE-1991-275',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 5, // ing electromecanica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IELE-208-82',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 5, // ing electromecanica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
         DB::table('plan_estudios')->insert([
             'clave' => 'IEME-1993-291',
             'is_actual' => false,
@@ -113,6 +196,15 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
+        //---------------------------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IE-73-040',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 17, // ing electromecanica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        // -------------
 
         DB::table('plan_estudios')->insert([
             'clave' => 'IGEM-2009-201',
@@ -122,6 +214,20 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IIND-1985-249',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 7, // ing industrial
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IIND-1990-265',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 7, // ing industrial
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
 
         DB::table('plan_estudios')->insert([
             'clave' => 'IIND-1993-297',
@@ -145,6 +251,23 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
+        //-------
+        DB::table('plan_estudios')->insert([
+            'clave' => 'II-73-025',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 18, // ing industrial
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'II-80-025',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 18, // ing industrial
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        //-------
+
 
         DB::table('plan_estudios')->insert([
             'clave' => 'IINF-2010-220',
@@ -164,6 +287,13 @@ class PlanDeEstudiosTableSeeder extends Seeder
         ]);
 
 
+        DB::table('plan_estudios')->insert([
+            'clave' => 'IMEC-1985-250',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 10, // ing mecanica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
         DB::table('plan_estudios')->insert([
             'clave' => 'IMEC-1993-298',
             'is_actual' => false,
@@ -209,6 +339,20 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s")
         ]);
 
+        DB::table('plan_estudios')->insert([
+            'clave' => 'LC-77-005',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 12, // lic en contaduria
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'LCON-1992-284',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 12, // lic en contaduria
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
 
         DB::table('plan_estudios')->insert([
             'clave' => 'LCON-1993-302',
@@ -224,10 +368,29 @@ class PlanDeEstudiosTableSeeder extends Seeder
             'id_especialidad' => 12, // lic en contaduria
             'created_at' => date("Y-m-d H:i:s")
         ]);
-
-
+        DB::table('plan_estudios')->insert([
+            'clave' => 'LI-80-95',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 13, // lic en informatica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'LINF-1990-267',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 13, // lic en informatica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
         DB::table('plan_estudios')->insert([
             'clave' => 'LINF-1993-303',
+            'is_actual' => false,
+            'estado' => true,
+            'id_especialidad' => 13, // lic en informatica
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+        DB::table('plan_estudios')->insert([
+            'clave' => 'LINF-2004-303',
             'is_actual' => false,
             'estado' => true,
             'id_especialidad' => 13, // lic en informatica

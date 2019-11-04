1515440b0de7aad56ad00ad597ff5337a60c65f4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-11-03 19:49:13 -0600

    Cambios en BaseController
    Se han creado dos metodos mas para la creacion de strings de fechas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,17 +10,48 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+    protected $meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function formatDateHumanSpanish($date) {
-        $meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
-            "julio", "agosto", "septiembre", "octubre", "novimbre", "diciembre"];
-
+    private function dateInitialization($date) {
         $fechaGeneracion = $date->format("d-m-Y");
         $fechaGeneracionArray = explode("-", $fechaGeneracion);
-        $fecha_g_spanish = $fechaGeneracionArray[0]." de "
-            .$meses[Carbon::now()->month-1]." de "
-            .$fechaGeneracionArray[2];
+        return $fechaGeneracionArray;
+    }
+    /**
+     * Genera una fecha con formato en español
+     */
+    public function formatDateHumanSpanish($date) {
+
+        $fecha_g_spanish = $this->dateInitialization($date)[0]." de "
+            .$this->meses[Carbon::now()->month-1]." de "
+            .$this->dateInitialization($date)[2];
+
+        return $fecha_g_spanish;
+    }
+
+    /**
+     * Genera una fecha de formato de ejemplo: 15 dias del mes de octubre de 2021 version 2
+     */
+    public function formatDateHumanSpanishForActa($date) {
+        
+        $fecha_g_spanish = $this->dateInitialization($date)[0]." días del mes de "
+            .$this->meses[Carbon::now()->month-1]." de "
+            .$this->dateInitialization($date)[2];
+
+        return $fecha_g_spanish;
+    }
+
+    /**
+     * Genera una fecha de formato de ejemplo: 15 dias del mes de octubre de 2021 version 2
+     */
+    public function formatDateHumanSpanishForActa2($date) {
+        
+        $fecha_g_spanish = $this->dateInitialization($date)[0]." del mes de "
+            .$this->meses[Carbon::now()->month-1]." de "
+            .$this->dateInitialization($date)[2];
 
         return $fecha_g_spanish;
     }
